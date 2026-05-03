013d4f8f2952fe7458e4d6200409a05602fac903
manaheeljamil <manaheelsmit@gmail.com>
2026-05-03 15:25:29 +0500

    initial commit

--- api/index.js
+++ api/index.js
@@ -7,6 +7,17 @@ app.get('/', (req , res) =>{
     res.send('Hello World!');
 })
 
+const products = [
+    {id: 1, name: 'Product 1', price: 10.99},
+    {id: 2, name: 'Product 2', price: 19.99},
+    {id: 3, name: 'Product 3', price: 5.99}
+];
+
+app.get('/api', (req , res) =>{
+    res.json(products);
+})
+
+
 app.listen(port, () => {
     console.log('Server is running on port ' + port);
 })
